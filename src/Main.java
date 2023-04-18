9846dfb6e8fbc6a57ef6d89a7b5e6a411124f8f1
Tynchtykbek <C:\Users\\AppData\Roaming\The Bat!>
2023-04-18 13:32:03 +0600

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        agePersonAndOutsideTemperature(18, 4);
-        agePersonAndOutsideTemperature(21, -17);
-        agePersonAndOutsideTemperature(15, 15);
-        agePersonAndOutsideTemperature(45, 22);
-        agePersonAndOutsideTemperature(46, -12);
+        System.out.println(agePersonAndOutsideTemperature(18, 4));
+        System.out.println(agePersonAndOutsideTemperature(23, -5));
+        System.out.println(agePersonAndOutsideTemperature(34, 13));
+        System.out.println(agePersonAndOutsideTemperature(25, 10));
+        System.out.println(agePersonAndOutsideTemperature(40, -30));
 
     }
 
     public static String agePersonAndOutsideTemperature(int agePerson, int OutsideTemperature) {
 
         if (agePerson >= 20 && agePerson <= 45 && (OutsideTemperature >= -20 && OutsideTemperature <= 30)) {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
         }
         else if (agePerson <= 20 && (OutsideTemperature >= 0 && OutsideTemperature <= 28) ) {            {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
+
         }
-        } else if (agePerson >= 45) {
-            if (OutsideTemperature >= -10 && OutsideTemperature <= 25) {
-                System.out.println("Можно идти гулять");
+        } else if (agePerson > 45 && OutsideTemperature >= -10 && OutsideTemperature <= 25) {
+                return "Оставайтесь дома";
             } else {
-                System.out.println("Оставайтесь дома!");
+                return "Оставайтесь дома!";
             }
         }
-        return "Возраст человека = " + agePerson + " Температура на улице = " + OutsideTemperature;
     }
-}
