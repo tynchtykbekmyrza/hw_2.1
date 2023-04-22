3987c155c0e361ba207678bc2ce6f387c5245333
Tynchtykbek <C:\Users\\AppData\Roaming\The Bat!>
2023-04-22 23:57:36 +0600

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
+import java.util.Random;
+
 public class Main {
+    public static  int generateRandomAgePerson() {
+        Random random1 = new Random();
+        int agePerson = random1.nextInt(0, 80);
+        return agePerson;
+    }
     public static void main(String[] args) {
+
         System.out.println(agePersonAndOutsideTemperature(18, 4));
-        System.out.println(agePersonAndOutsideTemperature(23, -5));
+        System.out.println(agePersonAndOutsideTemperature(generateRandomAgePerson(), -5));
         System.out.println(agePersonAndOutsideTemperature(34, 13));
         System.out.println(agePersonAndOutsideTemperature(25, 10));
-        System.out.println(agePersonAndOutsideTemperature(40, -30));
+        System.out.println(agePersonAndOutsideTemperature(generateRandomAgePerson(), -30));
 
     }
 
     public static String agePersonAndOutsideTemperature(int agePerson, int OutsideTemperature) {
+        generateRandomAgePerson();
 
         if (agePerson >= 20 && agePerson <= 45 && (OutsideTemperature >= -20 && OutsideTemperature <= 30)) {
             return "Можно идти гулять";
@@ -21,6 +30,8 @@ public class Main {
                 return "Оставайтесь дома";
             } else {
                 return "Оставайтесь дома!";
+
+
             }
         }
     }
